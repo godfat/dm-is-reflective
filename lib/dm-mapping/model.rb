e65a1d879c3e50b18363108f7fd3cb67daa45339
godfat <godfat@godfat.org>
2008-08-04 01:02:40 +0800

    * updated README. * updated implementation in mapping. one travel for all.

--- lib/dm-mapping/model.rb
+++ lib/dm-mapping/model.rb
@@ -21,44 +21,23 @@ module DataMapper
       DataMapper.ensure_required_dm_mapping_adapter
       targets << Mapping::All if targets.empty?
 
-      targets.each{ |target|
-        case target
-          when Regexp;
-            mapping_regexp target
-
-          when Symbol, String;
-            mapping_symbol target.to_sym
-
-          when Class;
-            mapping_class target
-
-          else
-            raise ArgumentError.new("invalid argument: #{target.inspect}")
-        end
-      }
-    end
-
-    private
-    def mapping_regexp regexp
       map_fields{ |name, type, attrs|
-        property name.to_sym, type, attrs if name =~ regexp
+        targets.each{ |target|
+          case target
+            when Regexp;
+              property name.to_sym, type, attrs if name =~ target
+
+            when Symbol, String;
+              property name.to_sym, type, attrs if name == target.to_s
+
+            when Class;
+              property name.to_sym, type, attrs if type == target
+
+            else
+              raise ArgumentError.new("invalid argument: #{target.inspect}")
+          end
+        }
       }
     end
-
-    def mapping_symbol symbol
-      map_fields{ |name, type, attrs|
-        if name.to_sym == symbol
-          property symbol, type, attrs
-          break
-        end
-      }
-    end
-
-    def mapping_class klass
-      map_fields{ |name, type, attrs|
-        property name.to_sym, type, attrs if type == klass
-      }
-    end
-
   end
 end
