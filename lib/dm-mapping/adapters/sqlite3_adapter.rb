79db91d00105a0200ede5ede6a64d285ae907cf2
godfat <godfat@godfat.org>
2008-08-14 21:48:36 +0800

    * reflect varchar(size) instead of default value.

--- lib/dm-mapping/adapters/sqlite3_adapter.rb
+++ lib/dm-mapping/adapters/sqlite3_adapter.rb
@@ -8,7 +8,7 @@ module DataMapper
       module Migration
         def storages
 # activerecord-2.1.0/lib/active_record/connection_adapters/sqlite_adapter.rb: 177
-          sql = <<-SQL
+          sql = <<-SQL.compress_lines
             SELECT name
             FROM sqlite_master
             WHERE type = 'table' AND NOT name = 'sqlite_sequence'
@@ -28,7 +28,11 @@ module DataMapper
             attrs[:nullable] = true if field.notnull != 0 && !attrs[:serial]
             attrs[:default] = field.dflt_value[1..-2] if field.dflt_value
 
-            [field.name, type, attrs.merge(chain.attributes)]
+            ergo = field.type.match(/\((\d+)\)/)
+            size = ergo && ergo[1].to_i
+            attrs[:size] = size if size
+
+            [field.name, type, chain.attributes.merge(attrs)]
           }
         end
       end
