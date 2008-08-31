ee29584c209a9dccf4d503a7f64ecd65e12b5a7e
godfat <godfat@godfat.org>
2008-09-01 00:21:28 +0800

    postgres adapter added. const guard in test added.

--- test/test_dm-mapping.rb
+++ test/test_dm-mapping.rb
@@ -9,18 +9,31 @@ require 'test/abstract'
   end
 }
 
+# cost 1 second to run
 class Sqlite3Test < Test::Unit::TestCase
   include Abstract
 
   def setup_data_mapper
     DataMapper.setup(:default, 'sqlite3:tmp.sqlite3')
   end
-end
+end if defined?(DataObjects::Sqlite3)
 
+
+# cost 2 seconds to run
 class MysqlTest < Test::Unit::TestCase
   include Abstract
 
   def setup_data_mapper
     DataMapper.setup(:default, 'mysql://dm-mapping:godfat@localhost/dm-mapping')
   end
-end
+end if defined?(DataObjects::Mysql)
+
+
+# cost 3 seconds to run
+class PostgresTest < Test::Unit::TestCase
+  include Abstract
+
+  def setup_data_mapper
+    DataMapper.setup(:default, 'postgres://dm-mapping:godfat@localhost/dm-mapping')
+  end
+end if defined?(DataObjects::Postgres)
