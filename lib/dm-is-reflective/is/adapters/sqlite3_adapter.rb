e17d93b9d2210f1e4d8f52f2a712a17bc0eba943
Lin Jen-Shin <godfat@godfat.org>
2009-06-21 15:56:53 +0800

    remove comments

--- lib/dm-is-reflective/is/adapters/sqlite3_adapter.rb
+++ lib/dm-is-reflective/is/adapters/sqlite3_adapter.rb
@@ -5,13 +5,11 @@ module DataMapper
   module Is::Reflective
     module Sqlite3Adapter
       def storages
-# activerecord-2.1.0/lib/active_record/connection_adapters/sqlite_adapter.rb: 177
         sql = <<-SQL.compress_lines
           SELECT name
           FROM sqlite_master
           WHERE type = 'table' AND NOT name = 'sqlite_sequence'
         SQL
-# activerecord-2.1.0/lib/active_record/connection_adapters/sqlite_adapter.rb: 181
 
         query sql
       end
