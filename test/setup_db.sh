32a61fb1d4889a27f98ad7a48df3b4412abfae10
Lin Jen-Shin <godfat@godfat.org>
2009-11-27 23:02:12 +0800

    [test/setup_db.sh] i can't speak SQL.. :( fixed mysql sql

--- test/setup_db.sh
+++ test/setup_db.sh
@@ -7,7 +7,6 @@ createdb --user=postgres 'dm_is_reflective'
 psql --user=postgres -c  'ALTER DATABASE dm_is_reflective OWNER TO dm_is_reflective'
 
 # mysql
-mysql5 -u root -p -e 'CREATE USER dm_is_reflective'
-mysql5 -u root -p -e 'GRANT USAGE ON *.* TO dm_is_reflective@localhost IDENTIFIED BY "godfat"'
+mysql5 -u root -p -e 'GRANT USAGE ON dm_is_reflective.* TO dm_is_reflective@localhost IDENTIFIED BY "godfat"'
 mysql5 -u root -p -e 'CREATE DATABASE dm_is_reflective'
 mysql5 -u root -p -e 'GRANT ALL PRIVILEGES ON dm_is_reflective.* TO "dm_is_reflective"'
