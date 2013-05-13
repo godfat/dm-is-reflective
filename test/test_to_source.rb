52e05bc06362e3e5985301f89db4b326b28c171b
Lin Jen-Shin <godfat@godfat.org>
2013-05-13 18:51:21 +0800

    prefix test

--- test/test_to_source.rb
+++ test/test_to_source.rb
@@ -26,4 +26,27 @@ property :body, DataMapper::Property::Text, {:primitive=>String, :lazy=>true, :l
 end
     RUBY
   end
+
+  should 'match Abstract::Comment::Abstract::Comment' do
+    Abstract::Comment.to_source(Abstract::Comment).should.eq <<-RUBY
+class Abstract::Comment::Abstract::Comment < Object
+  include DataMapper::Resource
+  property :id, DataMapper::Property::Serial, {:primitive=>Integer, :min=>1, :serial=>true}
+property :title, DataMapper::Property::String, {:primitive=>String, :length=>50, :default=>"default title", :allow_nil=>false}
+property :body, DataMapper::Property::Text, {:primitive=>String, :lazy=>true, :length=>65535}
+end
+    RUBY
+  end
+
+  should 'match Abstract::Comment::Abstract::User' do
+    Abstract::User.to_source('Abstract::Comment').should.eq <<-RUBY
+class Abstract::Comment::Abstract::User < Object
+  include DataMapper::Resource
+  property :id, DataMapper::Property::Serial, {:primitive=>Integer, :min=>1, :serial=>true}
+property :login, DataMapper::Property::String, {:primitive=>String, :length=>70}
+property :sig, DataMapper::Property::Text, {:primitive=>String, :lazy=>true, :length=>65535}
+property :created_at, DataMapper::Property::DateTime, {:primitive=>DateTime}
+end
+    RUBY
+  end
 end
