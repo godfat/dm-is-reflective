dec0d59e7be9bfd463187582f08fc1a94998818f
Lin Jen-Shin <godfat@godfat.org>
2013-05-20 22:22:38 +0800

    now mysql supports multiple composite keys

--- lib/dm-is-reflective/adapters/mysql_adapter.rb
+++ lib/dm-is-reflective/adapters/mysql_adapter.rb
@@ -9,22 +9,50 @@ module DmIsReflective::MysqlAdapter
   private
   # construct needed table metadata
   def reflective_query_storage storage
-    sql = <<-SQL
-      SELECT c.column_name, c.column_key, c.column_default, c.is_nullable,
-             c.data_type, c.character_maximum_length, c.extra, c.table_name,
-             s.index_name
-      FROM      `information_schema`.`columns` c
-      LEFT JOIN `information_schema`.`statistics` s
-      ON       c.column_name = s.column_name
-      WHERE    c.table_schema = ? AND c.table_name = ?
-      GROUP BY c.column_name;
+    sql_indices = <<-SQL
+      SELECT column_name, index_name, non_unique
+      FROM   `information_schema`.`statistics`
+      WHERE  table_schema = ? AND table_name = ?
+    SQL
+
+    sql_columns = <<-SQL
+      SELECT column_name, column_key, column_default, is_nullable,
+             data_type, character_maximum_length, extra, table_name
+      FROM      `information_schema`.`columns`
+      WHERE  table_schema = ? AND table_name = ?
     SQL
 
     # TODO: can we fix this shit in dm-mysql-adapter?
     path = (options[:path]     || options['path'] ||
             options[:database] || options['database']).sub('/', '')
 
-    select(Ext::String.compress_lines(sql), path, storage)
+    indices =
+    select(Ext::String.compress_lines(sql_indices), path, storage).
+      group_by(&:column_name)
+
+    select(Ext::String.compress_lines(sql_columns), path, storage).
+    map do |column|
+      if idx = indices[column.column_name]
+        idx_uni, idx_com = idx.partition{ |i| i.non_unique == 0 }.map{ |i|
+          if i.empty?
+            nil
+          elsif i.size == 1
+            i.first.index_name.to_sym
+          else
+            i.map{ |ii| ii.index_name.to_sym }
+          end
+        }
+      else
+        idx_uni, idx_com = nil
+      end
+
+      column.instance_eval <<-RUBY
+        def unique_index; #{idx_uni.inspect}; end
+        def index       ; #{idx_com.inspect}; end
+      RUBY
+
+      column
+    end
   end
 
   def reflective_field_name field
@@ -36,23 +64,21 @@ module DmIsReflective::MysqlAdapter
   end
 
   def reflective_attributes field, attrs = {}
-    attrs[:serial] = true if field.extra      == 'auto_increment'
+    attrs[:serial] = true if field.extra == 'auto_increment'
 
-    case field.column_key
-    when 'PRI'
+    if field.column_key == 'PRI'
       attrs[:key] = true
       attrs[:unique_index] = :"#{field.table_name}_pkey"
-    when 'UNI'
-      attrs[:unique_index] = :"#{field.index_name}"
-    when 'MUL'
-      attrs[:index]        = :"#{field.index_name}"
+    else
+      attrs[:unique_index] = field.unique_index if field.unique_index
+      attrs[       :index] = field.       index if field.       index
     end
 
     attrs[:allow_nil] = field.is_nullable == 'YES'
-    attrs[:default]  = field.column_default           if
+    attrs[:default]   = field.column_default           if
       field.column_default
 
-    attrs[:length]   = field.character_maximum_length if
+    attrs[:length]    = field.character_maximum_length if
       field.character_maximum_length
 
     attrs
